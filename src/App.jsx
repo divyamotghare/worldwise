bcbc2fda27be8ae00f39051cd1f396783d346e9e
Divya Motghare <divyamotghare9@gmail.com>
2024-01-22 21:18:45 +0530

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import HomePage from "./pages/HomePage";
+import PageNotFound from "./pages/PageNotFound";
+
 function App() {
-  return <div>Worldwise</div>;
+  return (
+    <div>
+      <h2>Hello Router</h2>
+      <BrowserRouter>
+        <Routes>
+          <Route path="product" element={<Product />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
