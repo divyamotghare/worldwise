89a1b3ea2c408d338e04550f331dc5b7809d30ac
Divya Motghare <divyamotghare9@gmail.com>
2024-02-12 23:37:13 +0530

    added all the routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import HomePage from "./pages/HomePage";
 import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
 
 function App() {
   return (
@@ -10,9 +11,10 @@ function App() {
       <h2>Hello Router</h2>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<HomePage />} />
           <Route path="product" element={<Product />} />
           <Route path="pricing" element={<Pricing />} />
-          <Route path="/" element={<HomePage />} />
+          <Route path="app" element={<AppLayout />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
