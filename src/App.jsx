dfa58db720c4b28d522784c3d2de55821786d0b1
Divya Motghare <divyamotghare9@gmail.com>
2024-02-13 11:15:12 +0530

    refractored files

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,17 @@ import Pricing from "./pages/Pricing";
 import HomePage from "./pages/HomePage";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <div>
-      <h2>Hello Router</h2>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="product" element={<Product />} />
           <Route path="pricing" element={<Pricing />} />
+          <Route path="/login" element={<Login />} />
           <Route path="app" element={<AppLayout />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
