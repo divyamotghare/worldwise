45925e2a3f2cf951baac8a204245441452c7701b
Divya Motghare <divyamotghare9@gmail.com>
2024-02-19 17:11:38 +0530

    created context API

---
+++ src/Contexts/CitiesContext.jsx
@@ -0,0 +1,46 @@
+import { useContext } from "react";
+import { createContext, useState, useEffect } from "react";
+
+const BASE_URL = "http://localhost:8000";
+
+const CitiesContext = createContext();
+
+function CitiesProvider({ children }) {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("there was an error loading data...");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+  return (
+    <CitiesProvider
+      value={{
+        cities,
+        isLoading,
+      }}
+    >
+      {children}
+    </CitiesProvider>
+  );
+}
+
+function useCities() {
+  const context = useContext(CitiesContext);
+  if (context === undefined)
+    throw new Error("CitiesContext is used outside the CitiesProvider");
+  return context;
+}
+
+export { CitiesProvider, useCities };
