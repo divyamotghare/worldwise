fe442011f168de914a758b0d88b6480158f82817
Divya Motghare <divyamotghare9@gmail.com>
2024-03-06 11:46:55 +0530

    custom hook useCities

---
+++ src/Contexts/useCities.jsx
@@ -0,0 +1,9 @@
+import { useContext } from "react";
+import { CitiesContext } from "./citiesContext";
+
+export function useCities() {
+  const context = useContext(CitiesContext);
+  if (context === undefined)
+    throw new Error("CitiesContext is used outside the CitiesProvider");
+  return context;
+}
