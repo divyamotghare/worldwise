4c359c7a0e4e18744b80be89fba3aa8dc588d1f1
Divya Motghare <divyamotghare9@gmail.com>
2024-03-20 05:29:49 +0530

    added a createCity function for post method

--- src/Contexts/CitiesContext.jsx
+++ src/Contexts/CitiesContext.jsx
@@ -37,6 +37,26 @@ function CitiesProvider({ children }) {
       setIsLoading(false);
     }
   }
+
+  async function createCity(newCity) {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/cities`, {
+        method: "POST",
+        body: JSON.stringify(newCity),
+        headers: {
+          "content-Type": "application/json",
+        },
+      });
+      const data = await res.json();
+
+      setCities((cities) => [...cities, data]);
+    } catch {
+      alert("there was an error loading data...");
+    } finally {
+      setIsLoading(false);
+    }
+  }
   return (
     <CitiesContext.Provider
       value={{
@@ -44,6 +64,7 @@ function CitiesProvider({ children }) {
         isLoading,
         currentCity,
         getCity,
+        createCity,
       }}
     >
       {children}
